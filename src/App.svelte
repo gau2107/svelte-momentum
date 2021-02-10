98aa88bf6e7864a5550580d326f0593dc370d131
Solanki Gaurav <solankigaurav2107@gmail.com>
2021-02-10 09:36:41 +0530

    quote component seperated from parent

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,19 @@
 <script>
+import Quote from "./Quote.svelte";
+
 	// TODO move into seperate components
 	let image = null;
-	let quote = null;
 	getBackgroundImage();
-	getQuote();
 	async function getBackgroundImage () {
 		const res = await fetch('https://picsum.photos/1280/720.webp', {
 			method: 'GET'
 		})
 		image = await res.url;
 	}
-	async function getQuote () {
-		const res = await fetch('https://quotes.rest/qod?language=en', {
-			method: 'GET',
-			accept: 'application/json'
-		})
-		let resInJson = await res.json();
-		quote = resInJson.contents.quotes[0];
-	}
 </script>
 <!-- BUG remove upper white space -->
 {#if image}
 <body style="background-image: url('{image}')">
-	<h1>{quote.quote}</h1>
+	<Quote />
 </body>
 {/if}
---
+++ src/Quote.svelte
@@ -0,0 +1,15 @@
+<script>
+    let quote = null;
+    getQuote();
+    async function getQuote() {
+        const res = await fetch('https://quotes.rest/qod?language=en', {
+			method: 'GET',
+			accept: 'application/json'
+		})
+		let resInJson = await res.json();
+		quote = resInJson.contents.quotes[0];
+    }
+</script>
+{#if quote}
+    <h1>{quote.quote} - {quote.author}</h1>
+{/if}
