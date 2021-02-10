3cedb3c1cc67c56c742cd0641bf2834b38b45cf1
Solanki Gaurav <solankigaurav2107@gmail.com>
2021-02-10 11:52:57 +0530

    get background from quotes api instead of seperate api

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,20 @@
 <script>
 import Quote from "./Quote.svelte";
-
-	// TODO move into seperate components
-	let image = null;
+	let quote = null;
 	getBackgroundImage();
+
 	async function getBackgroundImage () {
-		const res = await fetch('https://picsum.photos/1280/720.webp', {
-			method: 'GET'
+		const res = await fetch('https://quotes.rest/qod?language=en', {
+			method: 'GET',
+			accept: 'application/json'
 		})
-		image = await res.url;
+		let resInJson = await res.json();
+		quote = resInJson.contents.quotes[0];
 	}
 </script>
 <!-- BUG remove upper white space -->
-{#if image}
-<body style="background-image: url('{image}')">
-	<Quote />
+{#if quote}
+<body style="background-image: url('{quote.background}')">
+	<Quote quote={quote} />
 </body>
 {/if}
