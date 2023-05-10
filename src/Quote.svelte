0bc0770eab90a4ba9d08d6c044b8824f4e71b25f
Solanki Gaurav <solankigaurav2107@gmail.com>
2023-05-10 10:45:26 +0530

    change app name and change quote api

--- src/Quote.svelte
+++ src/Quote.svelte
@@ -2,15 +2,15 @@
   let quote = null;
   getQuote();
   async function getQuote() {
-    const res = await fetch("https://quotes.rest/qod?language=en", {
+    const res = await fetch("https://api.quotable.io/random", {
       method: "GET",
       accept: "application/json",
     });
     let resInJson = await res.json();
-    quote = resInJson.contents.quotes[0];
+    quote = resInJson;
   }
 </script>
 
 {#if quote}
-  <span class="quote">{quote.quote} - {quote.author}</span>
+  <span class="quote">{quote.content} - {quote.author}</span>
 {/if}
