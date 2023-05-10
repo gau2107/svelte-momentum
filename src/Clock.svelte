737669ac4a4f9ed1c3345a173fd92c7e2bba9f7f
Solanki Gaurav <solankigaurav2107@gmail.com>
2023-05-10 12:22:24 +0530

    [bug]: smooth transition for clock and pomodoro

--- src/Clock.svelte
+++ src/Clock.svelte
@@ -1,13 +1,17 @@
 <script>
-  let curTime = null;
+  let curTime = incrementTime();
   setInterval(() => {
+    curTime = incrementTime();
+  }, 1000);
+
+  function incrementTime() {
     let cur = new Date();
     let h = cur.getHours().toString();
     h = h.length === 1 ? "0" + h : h;
     let m = cur.getMinutes().toString();
     m = m.length === 1 ? "0" + m : m;
-    curTime = h + ":" + m;
-  }, 1000);
+    return h + ":" + m;
+  }
 </script>
 
 {#if curTime}
--- src/Pomodoro.svelte
+++ src/Pomodoro.svelte
@@ -1,15 +1,19 @@
 <script>
   let timer = 1500;
-  let displayTime = "";
+  let displayTime = setDisplayTime();
 
   const pomodoro = setInterval(() => {
-    timer -= 1;
-    let m = parseInt(timer / 60);
-    let s = timer % 60;
-    displayTime = (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s);
+    displayTime = setDisplayTime();
     timer === 0 && playSound();
     timer === 0 && clearInterval(pomodoro);
   }, 1000);
+
+  function setDisplayTime() {
+    timer -= 1;
+    let m = parseInt(timer / 60);
+    let s = timer % 60;
+    return (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s);
+  }
 
   function playSound() {
     const audio = new Audio("/assets/audio/notification.mp3");
